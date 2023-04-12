1ec89a3427312d257a667dcce6498029e31f97ee
Arjunaaaaa <114505560+Arjunaaaaa@users.noreply.github.com>
2023-04-12 12:42:20 +0800

    Create aspekrasio.js

---
+++ aspekrasio.js
@@ -0,0 +1,9 @@
+const video = document.getElementById("vidio");
+      function fill () {
+        video.classList.remove("fit");
+        video.classList.add("fill");
+      }
+      function fit () {
+        video.classList.remove("fill");
+        video.classList.add("fit");
+      }
