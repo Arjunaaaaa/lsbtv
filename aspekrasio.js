5e55620a33700ee94e6da9d4fe3848faab5b7a90
Arjunaaaaa <114505560+Arjunaaaaa@users.noreply.github.com>
2023-05-11 22:57:33 +0800

    aspekrasio.js

--- aspekrasio.js
+++ aspekrasio.js
@@ -3,7 +3,29 @@ const video = document.getElementById("vidio");
         video.classList.remove("fit");
         video.classList.add("fill");
       }
+        function fill () {
+        video.classList.remove("fit");
+        video.classList.remove("zoomIn");
+        video.classList.remove("zoomOut");
+        video.classList.add("fill");
+      }
       function fit () {
         video.classList.remove("fill");
+        video.classList.remove("zoomIn");
+        video.classList.remove("zoomOut");
         video.classList.add("fit");
       }
+      
+      function zoomIn () {
+        video.classList.remove("fit");
+        video.classList.remove("fill");
+        video.classList.remove("zoomOut");
+        video.classList.add("zoomIn");
+      }
+      function zoomOut () {
+        video.classList.remove("fit");
+        video.classList.remove("zoomIn");
+        video.classList.remove("fill");
+        video.classList.add("zoomOut");
+      }
+
