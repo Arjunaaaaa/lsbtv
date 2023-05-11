01e624d08675fd0a6fbdaecdf4aeac6f64308c97
Arjunaaaaa <114505560+Arjunaaaaa@users.noreply.github.com>
2023-05-11 22:59:15 +0800

    popupwindow.js

--- popupwindow.js
+++ popupwindow.js
@@ -1,5 +1,13 @@
-window.onclick = function(event) {
-		if (event.target == document.getElementById('myModal')) {
-			document.getElementById('myModal').style.display = "none";
-		}
-	}
+
+const videoPlayer = document.getElementById('video');
+const fullscreenButton = document.getElementById('fullscreen-button');
+
+fullscreenButton.addEventListener('click', () => {
+  if (videoPlayer.requestFullscreen) {
+    videoPlayer.requestFullscreen();
+  } else if (videoPlayer.webkitRequestFullscreen) { /* Safari */
+    videoPlayer.webkitRequestFullscreen();
+  } else if (videoPlayer.msRequestFullscreen) { /* IE11 */
+    videoPlayer.msRequestFullscreen();
+  }
+});
